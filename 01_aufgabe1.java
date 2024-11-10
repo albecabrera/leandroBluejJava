971b771def3bca2af1c6b7d662db2dde2a794d99
albecabrera <albe.cabrera@yahoo.de>
2024-11-10 10:39:01 +0100

    first commit

---
+++ 01_aufgabe1.java
@@ -0,0 +1,6 @@
+public class MostrarPalabra {
+    public static void main(String[] args) {
+        // Mostrar la palabra 'abba'
+        System.out.println("abba");
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# leandroBluejJava
+
+## Description
+
